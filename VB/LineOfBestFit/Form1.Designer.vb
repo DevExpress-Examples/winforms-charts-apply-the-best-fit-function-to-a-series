bedd8f56999db420e19565e9364731286f7e8609
DevExpressExampleBot <fakeEmail>
2022-11-30 15:10:06 +0400

    Source auto update [skip ci]

---
+++ VB/LineOfBestFit/Form1.Designer.vb
@@ -0,0 +1,118 @@
+Namespace LineOfBestFit
+
+    Partial Class Form1
+
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim xyDiagram2 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel2 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim pointSeriesView3 As DevExpress.XtraCharts.PointSeriesView = New DevExpress.XtraCharts.PointSeriesView()
+            Dim pointSeriesView4 As DevExpress.XtraCharts.PointSeriesView = New DevExpress.XtraCharts.PointSeriesView()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            Me.button1 = New System.Windows.Forms.Button()
+            Me.textBox1 = New System.Windows.Forms.TextBox()
+            Me.label1 = New System.Windows.Forms.Label()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesView3), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesView4), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            xyDiagram2.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram2.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = xyDiagram2
+            Me.chartControl1.Location = New System.Drawing.Point(11, 22)
+            Me.chartControl1.Name = "chartControl1"
+            series2.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.Numerical
+            series2.Label = pointSeriesLabel2
+            series2.Name = "Series 1"
+            series2.View = pointSeriesView3
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series2}
+            Me.chartControl1.SeriesTemplate.View = pointSeriesView4
+            Me.chartControl1.Size = New System.Drawing.Size(693, 490)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' button1
+            ' 
+            Me.button1.Location = New System.Drawing.Point(35, 529)
+            Me.button1.Name = "button1"
+            Me.button1.Size = New System.Drawing.Size(235, 52)
+            Me.button1.TabIndex = 1
+            Me.button1.Text = "Show Line of Best Fit"
+            Me.button1.UseVisualStyleBackColor = True
+            AddHandler Me.button1.Click, New System.EventHandler(AddressOf Me.button1_Click)
+            ' 
+            ' textBox1
+            ' 
+            Me.textBox1.Location = New System.Drawing.Point(319, 561)
+            Me.textBox1.Name = "textBox1"
+            Me.textBox1.Size = New System.Drawing.Size(263, 20)
+            Me.textBox1.TabIndex = 2
+            ' 
+            ' label1
+            ' 
+            Me.label1.AutoSize = True
+            Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte((204))))
+            Me.label1.Location = New System.Drawing.Point(366, 529)
+            Me.label1.Name = "label1"
+            Me.label1.Size = New System.Drawing.Size(167, 20)
+            Me.label1.TabIndex = 3
+            Me.label1.Text = "Correlation coefficient:"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(716, 593)
+            Me.Controls.Add(Me.label1)
+            Me.Controls.Add(Me.textBox1)
+            Me.Controls.Add(Me.button1)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((xyDiagram2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesView3), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesView4), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+        End Sub
+
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+
+        Private button1 As System.Windows.Forms.Button
+
+        Private textBox1 As System.Windows.Forms.TextBox
+
+        Private label1 As System.Windows.Forms.Label
+    End Class
+End Namespace
---
+++ VB/LineOfBestFit/Form1.vb
@@ -0,0 +1,98 @@
+Imports System
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.Charts.Native
+Imports DevExpress.Utils
+Imports DevExpress.XtraCharts
+
+Namespace LineOfBestFit
+
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Shared r As Double
+
+        Private Shared Function GetPointForLinearRegression(ByVal series As Series, ByVal argument As Object, ByVal functionArguments As String(), ByVal values As DataSourceValues(), ByVal colors As Object()) As ISeriesPoint()
+            Dim points As SeriesPoint() = New SeriesPoint(1) {}
+            Dim mx As Double = 0
+            Dim my As Double = 0
+            Dim mxy As Double = 0
+            Dim mx2 As Double = 0
+            Dim sx As Double = 0
+            Dim sy As Double = 0
+            Dim a As Double = 0
+            Dim b As Double = 0
+            Dim corr As Double = 0
+            Dim minValue As Double = Double.MaxValue
+            Dim maxValue As Double = Double.MinValue
+            Dim n As Integer = series.Points.Count
+            For Each sp As SeriesPoint In series.Points
+                If Convert.ToDouble(sp.Argument) < minValue Then
+                    minValue = Convert.ToDouble(sp.Argument)
+                End If
+
+                If Convert.ToDouble(sp.Argument) > maxValue Then
+                    maxValue = Convert.ToDouble(sp.Argument)
+                End If
+
+                mx += Convert.ToDouble(sp.Argument)
+                my += sp.Values(0)
+                mxy += Convert.ToDouble(sp.Argument) * sp.Values(0)
+                mx2 += Math.Pow(Convert.ToDouble(sp.Argument), 2)
+            Next
+
+            Try
+                a =(my / mx - mxy / mx2) / (n / mx - mx / mx2)
+                b =(my / n - mxy / mx) / (mx / n - mx2 / mx)
+            Catch
+            End Try
+
+            points(0) = New SeriesPoint(minValue, New Double() {a + b * minValue})
+            points(1) = New SeriesPoint(maxValue, New Double() {a + b * maxValue})
+            mx = mx / n
+            my = my / n
+            For Each sp As SeriesPoint In series.Points
+                sx += Math.Pow(Convert.ToDouble(sp.Argument) - mx, 2)
+                sy += Math.Pow(sp.Values(0) - my, 2)
+                corr +=(Convert.ToDouble(sp.Argument) - mx) * (sp.Values(0) - my)
+            Next
+
+            sx = Math.Pow(sx / n, 0.5)
+            sy = Math.Pow(sy / n, 0.5)
+            corr = corr / (n * sx * sy)
+            r = corr
+            Return points
+        End Function
+
+        Public Sub New()
+            InitializeComponent()
+            chartControl1.RegisterSummaryFunction("BESTFIT", "BESTFIT", 1, Nothing, New SummaryFunction(AddressOf GetPointForLinearRegression))
+            Dim ds As DataSet = New DataSet()
+            Dim dt As DataTable = New DataTable("MyDataTable")
+            dt.Columns.Add("MyArg", GetType(Double))
+            dt.Columns.Add("MyData", GetType(Double))
+            Dim rnd As Random = New Random()
+            For x As Double = -2 To 9
+                dt.Rows.Add(New Object() {x, rnd.NextDouble() * 10 - 5})
+            Next
+
+            ds.Tables.Add(dt)
+            chartControl1.DataSource = ds.Tables("MyDataTable")
+            chartControl1.Series(0).ArgumentDataMember = "MyArg"
+            chartControl1.Series(0).ValueDataMembers.AddRange(New String() {"MyData"})
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim BestFit As Series = New Series("Line Of Best Fit", ViewType.Line)
+            BestFit.ArgumentScaleType = ScaleType.Numerical
+            BestFit.LabelsVisibility = DefaultBoolean.False
+            chartControl1.Series.Add(BestFit)
+            chartControl1.Series(1).ArgumentDataMember = "MyArg"
+            chartControl1.Series(1).ValueDataMembers.AddRange(New String() {"MyData"})
+            chartControl1.Series(1).SummaryFunction = "BESTFIT()"
+            textBox1.Text = r.ToString()
+        End Sub
+    End Class
+End Namespace
